35c713dacbbeda72def826c6ce4f0059b0e75e21
freegf <695943001@qq.com>
2015-10-10 11:42:30 +0800

    新建Activity

---
+++ app/src/main/java/com/zerovoid/shoppingcart/activity/ShoppingCartActivity.java
@@ -0,0 +1,16 @@
+package com.zerovoid.shoppingcart.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zerovoid.shoppingcart.R;
+
+public class ShoppingCartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+
+}
