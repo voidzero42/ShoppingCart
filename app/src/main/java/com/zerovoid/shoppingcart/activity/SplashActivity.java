69e9c348f7c208111df7932cfee7c2e317d4330d
freegf <695943001@qq.com>
2015-11-03 20:02:39 +0800

    1、增加闪屏；

--- app/src/main/java/com/zerovoid/shoppingcart/activity/SplashActivity.java
+++ app/src/main/java/com/zerovoid/shoppingcart/activity/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.splash);
         start();
     }
 
